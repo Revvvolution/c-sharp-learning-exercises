096d9cecc301fd067e102a61d67caffcb579eea0
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-07-29 15:41:02 -0400

    Phase 1 complete

--- plan-your-heist/Program.cs
+++ plan-your-heist/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace plan_your_heist
+{
+    public class TeamMember
+    {
+        public string? Name { get; set; }
+        public int SkillLevel { get; set; }
+        public double CourageFactor { get; set; }
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"     
+                    ************************
+                    *** Plan Your Heist! ***
+                    ************************");
+
+            TeamMember newTeamMember = GetTeamMemberInfo();
+            
+
+            Console.WriteLine($"\n\t\t\tNew Heist Team:\n\n {newTeamMember.Name} , Skill Level: {newTeamMember.SkillLevel} , Courage Factor: {newTeamMember.CourageFactor}");
+
+        }
+        static TeamMember GetTeamMemberInfo()
+        {
+            TeamMember teamMember = new TeamMember();
+
+            Console.Write("\nEnter team member's name (use a codename if possible): ");
+            teamMember.Name = Console.ReadLine() ?? string.Empty;
+
+            Console.WriteLine($"\nAlright. Now, what is {teamMember.Name}'s skill level?\n(Positive whole numbers only, please.)");
+            teamMember.SkillLevel = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine($"\n{teamMember.SkillLevel}, huh? Okay, what is {teamMember.Name}'s courage factor? (Scale = 0.0 to 2.0)");
+            teamMember.CourageFactor = double.Parse(Console.ReadLine()!);
+
+            return teamMember;
+
+        }
+
+        
+    }
+}
