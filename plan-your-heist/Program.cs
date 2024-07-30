885b2ffde940888c11d2feffae04ae4421ca98f2
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-07-30 11:54:58 -0400

    Added stored values for bank difficulty and sum of team SkillLevel

--- plan-your-heist/Program.cs
+++ plan-your-heist/Program.cs
@@ -12,16 +12,25 @@ namespace plan_your_heist
         public double CourageFactor { get; set; }
     };
 
+  
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine(@"     
-                    ************************
-                    *** Plan Your Heist! ***
-                    ************************");
+                        ************************
+                        *** Plan Your Heist! ***
+                        ************************");
 
+              
+            // Using a Dictionary to store the team member objects with their name as the Key.
             Dictionary<string, TeamMember> team = new Dictionary<string, TeamMember>();
+
+
+            // A List to store the SkillLevel Values separately
+            List<int> teamSkillLevels = new List<int>();
+
+            int bankDifficultyLevel = 100;
 
 
             while (true)
@@ -32,9 +41,15 @@ namespace plan_your_heist
                 {
                     break;
                 }
-                team[newTeamMember.Name] = newTeamMember;
+                team[newTeamMember.Name] = newTeamMember; // Adding Name key to Dictionary with TeamMember object as the value
+                teamSkillLevels.Add(newTeamMember.SkillLevel);  // Add SkillLevel to the separate list
                 Console.WriteLine($"\n\t\t\tNew Heist Member Added:\n\t   Name: {newTeamMember.Name} , Skill Level: {newTeamMember.SkillLevel} , Courage Factor: {newTeamMember.CourageFactor}\n\n");
+                
             };
+
+            // Get the Sum of all members' skill levels
+            int totalTeamSkillLevel = teamSkillLevels.Sum();
+
 
             Console.WriteLine($"\n\n\n\t\t\t***************************\n\t\t\t\tHeist Team:\n\t\t\t***************************  \n\t\t\t\t{team.Count} member(s)\n\n");
             foreach (var member in team.Values)
@@ -43,12 +58,17 @@ namespace plan_your_heist
                 
             }
 
+            // Display the team's total skill level and compare it to the bank's difficulty level
+            Console.WriteLine($"\n\n\t\t\tTotal Team Skill Level: {totalTeamSkillLevel}\n");
+
+
         }
+
         static TeamMember GetTeamMemberInfo()
         {
             TeamMember teamMember = new TeamMember();
 
-            Console.Write("\nEnter team member's (code)name ('ENTER' to cancel): ");
+            Console.WriteLine("\nEnter team member's (code)name ('ENTER' to cancel/complete):");
             teamMember.Name = Console.ReadLine();
 
             while (true)
